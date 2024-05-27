3279a5712db52e066ad76aa2ed914c4a76d7aa2c
Sahilbalhra <sahilbalhra0@gmail.com>
2024-05-27 17:14:17 +0530

    added global error handling

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,22 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
+import { HttpError } from "http-errors";
+import { config } from "./config/config";
 
 const app = express();
 
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to ebook apis." });
+});
+
+//Global Error Handler
+
+app.use((error: HttpError, req: Request, res: Response, next: NextFunction) => {
+    const statusCode = error.status || 500;
+
+    return res.status(statusCode).json({
+        message: error.message,
+        errorStack: config.env === "development" ? error.stack : "",
+    });
 });
 
 export default app;
--- src/config/config.ts
+++ src/config/config.ts
@@ -5,6 +5,7 @@ Config();
 const _config = {
     port: process.env.PORT,
     databaseurl: process.env.MONGO_CONNECTION_URL,
+    env: process.env.NODE_ENV,
 };
 
 export const config = Object.freeze(_config);
