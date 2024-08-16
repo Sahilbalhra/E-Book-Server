0ee20b80ae38772bcf3d81363c43033eecfd3e12
Sahilbalhra <sahilbalhra0@gmail.com>
2024-08-16 22:06:19 +0530

    added core for all

--- dist/src/app.js
+++ dist/src/app.js
@@ -5,34 +5,37 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
+// import cors, { CorsOptions } from "cors";
 const globalErrorHandler_1 = __importDefault(require("./middleware/globalErrorHandler"));
 const user_router_1 = __importDefault(require("./routes/user.router"));
 const book_router_1 = __importDefault(require("./routes/book.router"));
-const config_1 = require("./config/config");
+// import { config } from "./config/config";
 const review_router_1 = __importDefault(require("./routes/review.router"));
 const purchase_router_1 = __importDefault(require("./routes/purchase.router"));
 const dashboard_router_1 = __importDefault(require("./routes/dashboard.router"));
-const allowedOrigins = [
-    config_1.config.frontendDomain || "",
-    config_1.config.frontendDomain2 || "",
-];
-// CORS options
-const corsOptions = {
-    origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        }
-        else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true, // example for allowing cookies
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-};
+// const allowedOrigins: string[] = [
+//     config.frontendDomain || "",
+//     config.frontendDomain2 || "",
+// ];
+// // CORS options
+// const corsOptions: CorsOptions = {
+//     origin: (
+//         origin: string | undefined,
+//         callback: (err: Error | null, allow?: boolean) => void
+//     ) => {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+//     credentials: true, // example for allowing cookies
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+// };
 const app = (0, express_1.default)();
-app.use((0, cors_1.default)(corsOptions));
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/", (req, res) => {
     res.json({ message: "Welcome to ebook apis." });
