d320690f9cc7a67916af66284e1ff455a52dd609
Sahilbalhra <sahilbalhra0@gmail.com>
2024-08-16 18:29:38 +0530

    enabled core for all origin

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,39 @@
 import express from "express";
-import cors, { CorsOptions } from "cors";
+import cors from "cors";
+// import cors, { CorsOptions } from "cors";
 import globalErrorHandler from "./middleware/globalErrorHandler";
 import userRouter from "./routes/user.router";
 import bookRouter from "./routes/book.router";
-import { config } from "./config/config";
+// import { config } from "./config/config";
 import reviewRouter from "./routes/review.router";
 import purchaseRouter from "./routes/purchase.router";
 import dashboardRouter from "./routes/dashboard.router";
 
-const allowedOrigins: string[] = [
-    config.frontendDomain || "",
-    config.frontendDomain2 || "",
-];
+// const allowedOrigins: string[] = [
+//     config.frontendDomain || "",
+//     config.frontendDomain2 || "",
+// ];
 
-// CORS options
-const corsOptions: CorsOptions = {
-    origin: (
-        origin: string | undefined,
-        callback: (err: Error | null, allow?: boolean) => void
-    ) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true, // example for allowing cookies
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-};
+// // CORS options
+// const corsOptions: CorsOptions = {
+//     origin: (
+//         origin: string | undefined,
+//         callback: (err: Error | null, allow?: boolean) => void
+//     ) => {
+//         if (!origin || allowedOrigins.includes(origin)) {
+//             callback(null, true);
+//         } else {
+//             callback(new Error("Not allowed by CORS"));
+//         }
+//     },
+//     credentials: true, // example for allowing cookies
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//     preflightContinue: false,
+//     optionsSuccessStatus: 204,
+// };
 
 const app = express();
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
